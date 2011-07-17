edcbf4392a5771d43db8fc7160c8d56fc9a6fb96
Chester <cd@pobox.com>
2011-07-16 21:50:34 -0300

    output changed from html to json

--- myretweetedtweets_app/src/me/chester/myretweetedtweets/RetrieveTweetsServlet.java
+++ myretweetedtweets_app/src/me/chester/myretweetedtweets/RetrieveTweetsServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import twitter4j.TwitterException;
 
 import com.google.appengine.api.datastore.EntityNotFoundException;
+import static org.apache.commons.lang.StringEscapeUtils.escapeHtml;
 
 @SuppressWarnings("serial")
 public class RetrieveTweetsServlet extends HttpServlet {
@@ -20,19 +21,29 @@ public class RetrieveTweetsServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getPathInfo();
+		String callback = request.getParameter("callback");
 		PrintWriter out = response.getWriter();
 		if (path != null && path.length() > 1) {
 			try {
 				long id = Long.parseLong(path.substring(1));
 				User user = new User(id);
 				List<String> statuses = user.getMyRetweetedTweets();
-				out.write("<ul>");
-				for (String status : statuses) {
-					out.write("<li>");
-					out.write(status);
-					out.write("</li>");
+				if (callback != null) {
+					out.write(callback + "(");
 				}
-				out.write("</ul>");
+				out.write("{tweets:[");
+				boolean first = true;
+				for (String status : statuses) {
+					out.write(first ? "" : ",");
+					out.write("\"");
+					out.write(escapeHtml(status));
+					out.write("\"");
+					first = false;
+				}
+				out.write("]}");
+				if (callback != null) {
+					out.write(")");
+				}
 				return;
 			} catch (NumberFormatException e) {
 				// Invalid id, will be treated as 404-not-found below
--- myretweetedtweets_app/war/instructions.jsp
+++ myretweetedtweets_app/war/instructions.jsp
@@ -1,7 +1,7 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%
 	String id = (String) request.getAttribute("id");
-	String ul_url = "http://myretweetedtweets.appspot.com/ul/" + id;
+	String json_url = "http://myretweetedtweets.appspot.com/json/" + id;
 %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
@@ -12,9 +12,9 @@
 <body>
 	<h1>Cool, you've done it!</h1>
 	<p>You can use the following URL to retrieve your retweeted tweets
-		as an HTML list (&lt;ul&rt;):</p>
+		as a JSON object:</p>
 	<p>
-		<a href="<%=ul_url%>"><%=ul_url%></a>
+		<a href="<%=json_url%>"><%=json_url%></a>
 	</p>
 	<p>(remember this URL - if you forget it, you will need to redo the
 		authorization).</p>
